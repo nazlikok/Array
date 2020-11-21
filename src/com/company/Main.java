f49f006496a00d13b8e8477c9d608b1a20a1dcc0
nazlikok <nazlikok@hotmail.com>
2020-11-21 20:11:48 +0300

    2.nd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int max = 20;
+        int max = 10;
         int numMax = 19;
 
         int[] randNum = new int[max];
-        int[] temp = new int[randNum.length];
+        int end = randNum.length;
 
         System.out.println("First Random Array: ");
         for (int i = 0; i < randNum.length; i++) {
             randNum[i] = (int) (Math.random() * numMax + 1);
-            System.out.print(randNum[i] + "\t" );
+            System.out.print(randNum[i] + "\t");
 
         }
         System.out.println();
         System.out.println("Removed Duplicates Array: ");
+        for(int i=0; i< randNum.length;i++){
+            for(int j=i+1; j< randNum.length; j++){
+                if(randNum[i]>randNum[j]){
+                    int temp = randNum[i];
+                    randNum[i]=randNum[j];
+                    randNum[j]=temp;
+
+                }
+            }
+        }
+        for(int i=0;i<randNum.length;i++)
+        {
+            System.out.print(randNum[i]+"\t");
+        }
     }
 }
 
