9f0b1b3995a035cdc4898fd38f1e3a575d06b088
nazlikok <nazlikok@hotmail.com>
2020-11-21 20:16:31 +0300

    3.rd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int max = 10;
+        int max = 20;
         int numMax = 19;
 
         int[] randNum = new int[max];
@@ -16,7 +16,7 @@ public class Main {
 
         }
         System.out.println();
-        System.out.println("Removed Duplicates Array: ");
+        System.out.println("Sorted Array: ");
         for(int i=0; i< randNum.length;i++){
             for(int j=i+1; j< randNum.length; j++){
                 if(randNum[i]>randNum[j]){
@@ -28,6 +28,22 @@ public class Main {
             }
         }
         for(int i=0;i<randNum.length;i++)
+        {
+            System.out.print(randNum[i]+"\t");
+        }
+        int b=0;
+        randNum[b]=randNum[0];
+        for(int i=0;i<randNum.length;i++)
+        {
+            if (randNum[b]!=randNum[i])
+            {
+                b++;
+                randNum[b]=randNum[i];
+            }
+        }
+        System.out.println();
+        System.out.println("Removed Duplicates Array: ");
+        for (int i=0;i<=b;i++ )
         {
             System.out.print(randNum[i]+"\t");
         }
